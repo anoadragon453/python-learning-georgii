5f1583ec98d86a8a56eb3ce552c9b26b4cd06263
Andrew Morgan <andrew@amorgan.xyz>
2019-06-27 23:48:54 +0100

    Round 3

---
+++ README.md
@@ -0,0 +1,15 @@
+# Georgii's python adventures!
+
+## How to install dependencies
+
+```
+pip3 install -r requirements.txt
+```
+
+## How to run things
+
+Do:
+
+```
+python3 ./nameoffile.py
+```
--- store.py
+++ store.py
@@ -3,19 +3,25 @@
 # Have an interface where someone can do the following things:
 #
 # x See what items you have for sale and their cost
-# * Buy an item (their money goes down) [item goes out of stock once bought]
+# x Buy an item (their money goes down) [item goes out of stock once bought]
 # * Check their balance and their inventory
 # * Sell an item (their money goes up, but not as much as what they bought it for!)
 # * Leave the store (or try anyway)
+""" YO, MEATBAG! INSTALL PICK!"""
+
+from pick import pick
 
 """
     1) Create dictionary of stuff with prices (Store front)
     2) Create a dictionary for user to check what they have and how much money left
     3)
 """
+
 # Create an inventory of items, using a dictionary
-print("WELCOME TO BOB'S BUYOUT!")
-print("SOME PEOPLE CALL IT A TRASH, I CALL IT A GARBAGE!")
+customer_acc = 101
+customer_inventory = {}
+
+store_acc = 10000
 store_inventory = {
     "Apple": 9999,
     "Babana": 500,
@@ -24,26 +30,59 @@ store_inventory = {
     "House Rent": 100000000,
 }
 
-for item_presentation in store_inventory:
-    # When you loop through a dictionary, it loops through the "keys"
-    # value = some_dictionary[key]
 
-    # item_presentation = "Apple"
-    # want 9999
-    # store_inventory["Apple"] -> 9999
-    # store_inventory[item_presentation] -> price
-    price = store_inventory[item_presentation]
-    print(item_presentation, "%.2f" % (price), sep=": $")
-    # print(item_presentation + ": $" + price)
-    # Apple: $9999
+def show_inventory(inventory):
+    for item_presentation in inventory:
+        # When you loop through a dictionary, it loops through the "keys"
+        # value = some_dictionary[key]
+
+        # item_presentation = "Apple"
+        # want 9999
+        # store_inventory["Apple"] -> 9999
+        # store_inventory[item_presentation] -> price
+        price = inventory[item_presentation]
+        print(item_presentation, "%.2f" % (price), sep=": $")
+        # print(item_presentation + ": $" + price)
+        # Apple: $9999
+
+    # Pause to let the user look :D
+    input()
+
 
 # print("SOME PEOPLE CALL IT A TRASH, I CALL IT A GARBAGE!")
+title = "WELCOME TO BOB'S BUYOUT!"
+print("SOME PEOPLE CALL IT A TRASH, I CALL IT A GARBAGE!")
+options = ["See what is in the store", "Show what I got", "Buy something", "Go Home"]
+while customer_acc != 0:
+    print("WHAT'CHA WANT?!")
 
-print("WHAT'CHA WANT?!")
-user_input = input()
-if user_input == "See your inventory":
-    # print("SOME PEOPLE CALL IT A TRASH, I CALL IT A GARBAGE!")
-    # print(store_inventory)
-    pass
-else:
-    print("GO AWAY AND NEVER COME BACK!")
+    option, index = pick(options, title)
+
+    if index == 0:
+        show_inventory(store_inventory)
+    elif index == 1:
+        show_inventory(customer_inventory)
+    elif index == 2:
+        # ->>> show_inventory(store_inventory)
+        title = "WHAT'CHA WANT TO BUY?!"
+
+        counter = []
+        items = list(store_inventory.items())
+        for name, price in items:
+            counter.append("%s: $%.2f" % (name, price))
+
+        counter, index = pick(counter, title)
+        name, price = items[index]
+
+        print("BE MI GUST!")
+
+        customer_acc -= price
+        customer_inventory[name] = price / 2
+        store_acc += price
+        store_inventory.pop(name)
+    else:
+        print("GO AWAY AND NEVER COME BACK!")
+        break
+
+# TODO: Show how much money is had when you show an inventory
+# TODO: Don't let you buy things you can't afford!!!
